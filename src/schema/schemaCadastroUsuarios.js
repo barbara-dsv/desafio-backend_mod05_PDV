f6edce56e675aa724f638a9ab3cc2a3315b289a3
Ubirajara Santana <113365850+birasants@users.noreply.github.com>
2024-01-23 12:44:33 -0300

    :recycle: refactor: Ajustes
    Signed-off-by: Ubirajara Santana  <113365850+birasants@users.noreply.github.com>

--- src/schema/schemaCadastroUsuarios.js
+++ src/schema/schemaCadastroUsuarios.js
@@ -8,16 +8,16 @@ const schemaCadastroUsuario = joi.object({
     }),
 
     email: joi.string().pattern(/.*\S.*/).email().required().messages({
-		    'string.email'        :  'O campo email precisa ter um formato válido',
-		    'any.required'        :  'O campo email é obrigatório',
-		    'string.empty'        :  'O campo email é obrigatório',
+	'string.email'        :  'O campo email precisa ter um formato válido',
+	'any.required'        :  'O campo email é obrigatório',
+	'string.empty'        :  'O campo email é obrigatório',
         'string.pattern.base' :  'O campo email quando preenchido não pode ser encaminhado vazio.',
 	  }),
 
     senha: joi.string().min(5).pattern(/.*\S.*/).required().messages({
         'any.required'        :  'O campo senha é obrigatório',
-		    'string.empty'        :  'O campo senha é obrigatório',
-		    'string.min'          :  'A senha precisa conter, no mínimo, 5 caracteres',
+	'string.empty'        :  'O campo senha é obrigatório',
+	'string.min'          :  'A senha precisa conter, no mínimo, 5 caracteres',
         'string.pattern.base' :  'O campo senha quando preenchido não pode ser encaminhado vazio.',
 	  }),
 
