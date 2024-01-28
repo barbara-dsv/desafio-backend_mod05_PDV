f9339016f95e154622ad542f310411479642ab3f
Bárbara Lourenço Tavares <babisolua@gmail.com>
2024-01-28 20:46:56 -0300

    verificação com if caso for enviado email ou cpf para editar

--- src/controllers/cliente/editarCliente.js
+++ src/controllers/cliente/editarCliente.js
@@ -10,30 +10,34 @@ const editarCliente = async (req, res) => {
         if (!clienteExiste) {
             return res.status(404).json('cliente não encontrado')
         }
-        
-        const emailRegistrado = await knex('clientes').where({ email }).first()
+        if (email) {
+            const emailRegistrado = await knex('clientes').where({ email }).first()
 
-        if (emailRegistrado) {
-            return res.status(400).json({ mensagem: 'Email já cadastrado' })
+            if (emailRegistrado) {
+                return res.status(400).json({ mensagem: 'Email já cadastrado' })
+            }
         }
 
-        const cpfRegistrado = await knex('clientes').where({ cpf }).first()
+        if (cpf) {
+            const cpfRegistrado = await knex('clientes').where({ cpf }).first()
 
-        if (cpfRegistrado) {
-            return res.status(400).json({ mensagem: 'CPF já cadastrado' })
+            if (cpfRegistrado) {
+                return res.status(400).json({ mensagem: 'CPF já cadastrado' })
+            }
         }
 
         const cliente = await knex('clientes')
-        .update({nome, email, cpf, cep, rua, numero, bairro, cidade, estado})
-        .where({ id }).returning('*');
+            .update({ nome, email, cpf, cep, rua, numero, bairro, cidade, estado })
+            .where({ id }).returning('*');
 
-            if (!cliente) {
-                return res.status(404).json('não foi possivel atualizar o cliente');
-            }
+        if (!cliente) {
+            return res.status(404).json('não foi possivel atualizar o cliente');
+        }
 
-            return res.status(201).json(cliente[0])
+        return res.status(201).json(cliente[0])
 
     } catch (error) {
+        console.log(error.message)
         return res.status(500).json({ mensagem: 'Erro interno do servidor.' })
     }
 }
