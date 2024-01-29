cd188b7fc4ac02d35a236c228c9732d14cf653d2
Bárbara Lourenço Tavares <babisolua@gmail.com>
2024-01-29 14:24:12 -0300

    comentários retirados

--- src/rotas.js
+++ src/rotas.js
@@ -3,25 +3,25 @@ const express = require('express');
 const validacaoReq = require('./middleware/validacaoReq');
 const auth = require('./middleware/autenticacao');
 
-const schemaLoginUsuario  =  require('./schema/schemaLoginUsuario');
-const schemaUsuario       =  require('./schema/schemaUsuario');
-const schemaProduto       =  require('./schema/schemaProduto');
-const schemaCliente       =  require('./schema/schemaCliente');
+const schemaLoginUsuario = require('./schema/schemaLoginUsuario');
+const schemaUsuario = require('./schema/schemaUsuario');
+const schemaProduto = require('./schema/schemaProduto');
+const schemaCliente = require('./schema/schemaCliente');
 
-const detalharUsuario      =  require('./controllers/usuario/detalharUsuario');
-const { cadastrar }        =  require('./controllers/usuario/cadastrarUsuario');
-const { login }            =  require('./controllers/usuario/loginUsuario');
-const { editarUsuario }    =  require('./controllers/usuario/editarUsuario');
-const { listarCategoria }  =  require('./controllers/produto/listarCategorias');
-const { cadastrarProduto } =  require('./controllers/produto/cadastrarProduto');
-const { excluirProduto }   =  require('./controllers/produto/excluirProduto');
-const { listarClientes }   =  require('./controllers/cliente/listarClientes');
-const { cadastrarCliente } =  require('./controllers/cliente/cadastrarCliente');
-const { listarProdutos }   =  require('./controllers/produto/listarProdutos');
-const { editarProduto }    =  require('./controllers/produto/editarProduto');
-const { editarCliente }    =  require('./controllers/cliente/editarCliente');
-const { detalharCliente }  =  require('./controllers/cliente/detalharCliente')
-const { detalhar }         =  require('./controllers/produto/detalharProduto');
+const detalharUsuario = require('./controllers/usuario/detalharUsuario');
+const { cadastrar } = require('./controllers/usuario/cadastrarUsuario');
+const { login } = require('./controllers/usuario/loginUsuario');
+const { editarUsuario } = require('./controllers/usuario/editarUsuario');
+const { listarCategoria } = require('./controllers/produto/listarCategorias');
+const { cadastrarProduto } = require('./controllers/produto/cadastrarProduto');
+const { excluirProduto } = require('./controllers/produto/excluirProduto');
+const { listarClientes } = require('./controllers/cliente/listarClientes');
+const { cadastrarCliente } = require('./controllers/cliente/cadastrarCliente');
+const { listarProdutos } = require('./controllers/produto/listarProdutos');
+const { editarProduto } = require('./controllers/produto/editarProduto');
+const { editarCliente } = require('./controllers/cliente/editarCliente');
+const { detalharCliente } = require('./controllers/cliente/detalharCliente')
+const { detalhar } = require('./controllers/produto/detalharProduto');
 
 
 const rotas = express();
@@ -42,7 +42,7 @@ rotas.post('/produto', validacaoReq(schemaProduto), cadastrarProduto);
 
 rotas.get('/produto/:id', detalhar);
 
-rotas.put('/produto/:id',validacaoReq(schemaProduto), editarProduto);
+rotas.put('/produto/:id', validacaoReq(schemaProduto), editarProduto);
 
 rotas.get('/produtos', listarProdutos);
 
@@ -50,12 +50,11 @@ rotas.delete('/produto/:id', excluirProduto);
 
 rotas.post('/cliente', validacaoReq(schemaCliente), cadastrarCliente)
 
-//editar dados do cliente
-
 rotas.put('/cliente/:id', validacaoReq(schemaCliente), editarCliente)
+
 rotas.get('/clientes', listarClientes);
+
 rotas.get('/cliente/:id', detalharCliente)
 
-//detalhar cliente 
 
 module.exports = rotas
