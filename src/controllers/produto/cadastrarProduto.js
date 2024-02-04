573cd46ab5df861bc9bb5f99dcd6d9973cca1f3b
Bárbara Lourenço Tavares <babisolua@gmail.com>
2024-02-04 11:13:52 -0300

    código refatorado para receber arquivo de imagem com multer

--- src/controllers/produto/cadastrarProduto.js
+++ src/controllers/produto/cadastrarProduto.js
@@ -1,35 +1,52 @@
 const knex = require('../../db/conexao');
+const { uploadImagem } = require('../../servicos/uploads')
 
-const cadastrarProduto = async ( req, res ) =>{
-  const {descricao, quantidade_estoque, valor, categoria_id} = req.body;
+const cadastrarProduto = async (req, res) => {
+  const { descricao, quantidade_estoque, valor, categoria_id, imagem } = req.body;
 
   try {
 
-    const verificaCategoria = await knex( 'categorias' ).where('id', categoria_id).first();
+    const verificaCategoria = await knex('categorias').where('id', categoria_id).first();
 
-    if(!verificaCategoria){
+    if (!verificaCategoria) {
       return res.status(404).json({
         mensagem: "Categoria informada não existe."
-    })
-  }
+      })
+    }
 
-    const cadastraProduto = await knex('produtos').insert({
+    let cadastraProduto = await knex('produtos').insert({
       descricao,
       quantidade_estoque,
       valor,
       categoria_id
-  })
+    }).returning('*')
 
-    return res.status(201).json({
-      mensagem: "Produto cadastrado com sucesso!"
-  });
-  
+    const id = cadastraProduto[0].id
+    if (req.file) {
+      const { originalname, mimetype, buffer } = req.file
+
+      const imagem = await uploadImagem(
+        `produtos/${id}/${originalname}`,
+        buffer,
+        mimetype
+      )
+
+      cadastraProduto = await knex('produtos').update({
+        produto_imagem: imagem.url
+      }).where({ id }).returning('*')
+
+    }
+
+    return res.status(201).json(cadastraProduto[0]);
+
   } catch (error) {
+    console.log(error)
     return res.status(500).json({
-        mensagem: 'Erro interno do servidor.'
+      mensagem: 'Erro interno do servidor.'
     })
   }
 }
 
 module.exports = {
-  cadastrarProduto };
+  cadastrarProduto
+};
