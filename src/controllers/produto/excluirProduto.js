45689045300fd2a57976f9fdab68871441dd6e53
Bárbara Lourenço Tavares <babisolua@gmail.com>
2024-02-05 19:35:10 -0300

    código corrigido e refatorado para excluir imagem cadastrada para produto excluído

--- src/controllers/produto/excluirProduto.js
+++ src/controllers/produto/excluirProduto.js
@@ -1,14 +1,6 @@
 const knex = require('../../db/conexao');
-const aws = require('aws-sdk');
+const { excluirImagem } = require('../../servicos/uploads');
 
-
-const s3 = new aws.S3({
-    endpoint: new aws.Endpoint(`https://${process.env.ENDPOINT_BACKBLAZE}`),
-    credentials: {
-        accessKeyId: process.env.KEY_ID,
-        secretAccessKey: process.env.APP_KEY
-    }
-});
 const excluirProduto = async (req, res) => {
   const { id } = req.params;
 
@@ -23,36 +15,22 @@ const excluirProduto = async (req, res) => {
     }
 
     const pedidoCadastrado = await knex('pedido_produtos').where('produto_id', id).first();
-    
+
     if (pedidoCadastrado) {
       return res.status(400).json({
         mensagem: 'O produto está cadastrado em um pedido e não pode ser excluído.'
       });
     }
 
-    // Identificar o campo que contém o nome da imagem
-    let nomeCampoImagem;
-    for (const chave in produto) {
-      if (Object.prototype.hasOwnProperty.call(produto, chave) && typeof produto[chave] === 'string') {
-        nomeCampoImagem = chave;
-        break;
-      }
+    if (produto.produto_imagem !== null) {
+      await excluirImagem(produto.produto_imagem)
     }
-
-    // Obter o nome do arquivo da imagem associada ao produto
-    const imagemNome = produto[nomeCampoImagem];
 
     // Excluir o produto do banco de dados
     await knex('produtos').where({ id }).del();
 
-    // Se o nome da imagem existir, excluir a imagem usando a função exclusaoDeImagem
-    if (imagemNome) {
-      await exclusaodeImagem(imagemNome);
-    }
-
     return res.status(204).send();
   } catch (error) {
-    console.error(error);
     return res.status(500).json({
       mensagem: 'Erro interno do servidor.'
     });
